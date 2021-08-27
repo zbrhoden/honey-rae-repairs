6386ec2fefdd52899712aef993b07d285e0ad975
Blake Rhoden <zbrhoden@gmail.com>
2021-08-27 11:36:27 -0500

    customer message

--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 
 export const CustomerList = () => {
     const [customers, setCustomers] = useState([])
+    const [totalCustomerMessage, updateMessage] = useState("")
 
     useEffect(
         () => {
@@ -16,15 +17,28 @@ export const CustomerList = () => {
         []
     )
 
+    useEffect(
+        () => {
+            if (customers.length === 1) {
+                updateMessage("You have 1 customer")
+            }
+            else {
+                updateMessage(`You have ${customers.length} customers`)
+            }
+        },
+        [customers]
+    )
+
     return (
         <>
-        {
-            customers.map(
-                (customerObject) => {
-                    return <h2 key={`customer--${customerObject.id}`}>{customerObject.name}</h2>
-                }
-            )
-        }
+            <div>{totalCustomerMessage}</div>
+            {
+                customers.slice(0, 5).map(
+                    (customerObject) => {
+                        return <h2 key={`customer--${customerObject.id}`}>{customerObject.name}</h2>
+                    }
+                )
+            }
         </>
     )
 }
