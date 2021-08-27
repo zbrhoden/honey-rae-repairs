80cd088f9139ae4a16deff258f2607bdb65b8b5b
Blake Rhoden <zbrhoden@gmail.com>
2021-08-27 09:37:38 -0500

    added customers

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
+import { CustomerList } from "./customers/CustomerList"
 
 export const Repairs = () => {
-    const [customers, setCustomers] = useState([])
-
-    useEffect(
-        () => {
-            fetch("http://localhost:8088/customers")
-                .then(res => res.json())
-                .then(
-                    (customerArray) => { 
-                        setCustomers(customerArray)
-                    }
-                )
-        },
-        []
-    )
-
     return (
         <>
         <h1>Honey Rae's Repair Shop</h1>
-
-        {
-            customers.map(
-                (customerObject) => {
-                    return <h2>{customerObject.name}</h2>
-                }
-            )
-        }
+        <CustomerList/>
+        
         </>
     )
 }
---
+++ src/components/customers/CustomerList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react"
+
+export const CustomerList = () => {
+    const [customers, setCustomers] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/customers")
+                .then(res => res.json())
+                .then(
+                    (customerArray) => { 
+                        setCustomers(customerArray)
+                    }
+                )
+        },
+        []
+    )
+
+    return (
+        <>
+        {
+            customers.map(
+                (customerObject) => {
+                    return <h2 key={`customer--${customerObject.id}`}>{customerObject.name}</h2>
+                }
+            )
+        }
+        </>
+    )
+}
