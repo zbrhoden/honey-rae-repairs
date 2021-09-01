64f10cccaa2c2c142ae4f44f9d82e74fe6f90f6e
Blake Rhoden <zbrhoden@gmail.com>
2021-09-01 13:19:36 -0500

    Employees

--- src/components/Employees/EmployeeList.js
+++ src/components/Employees/EmployeeList.js
@@ -1,34 +1,25 @@
 import React, { useEffect, useState } from "react"
 
 export const EmployeeList = () => {
-    const [employees, changeEmployee] = useState([])
-    const [specialties, setSpecial] = useState("")
+    const [employees, setemployees] = useState([])
 
     useEffect(
         () => {
             fetch("http://localhost:8088/employees")
                 .then(res => res.json())
-                .then((employeesFromAPI) => {
-                    changeEmployee(employeesFromAPI)
+                .then((employeeArray) => {
+                    setemployees(employeeArray)
                 })
         },
         []
     )
 
-    useEffect(() => {
-        const justSpecialities = employees.map(emp => emp.specialty)
-        setSpecial(justSpecialities.join(", "))
-    }, [employees])
-
     return (
         <>
-            <div>
-                Specialties: { specialties }
-            </div>
             {
                 employees.map(
-                    (employee) => {
-                        return <p key={`employee--${employee.id}`}>{employee.name}</p>
+                    (employeeObject) => {
+                        return <h2 key={`employee--${employeeObject.id}`}>{employeeObject.name}</h2>
                     }
                 )
             }
