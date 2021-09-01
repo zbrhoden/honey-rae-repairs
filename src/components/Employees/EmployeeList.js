738c98723220f1be2fbcc01c673513a9657ffaa4
Blake Rhoden <zbrhoden@gmail.com>
2021-09-01 13:48:48 -0500

    added specialties

--- src/components/Employees/EmployeeList.js
+++ src/components/Employees/EmployeeList.js
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from "react"
 
 export const EmployeeList = () => {
-    const [employees, setemployees] = useState([])
+    const [employees, changeEmployee] = useState([])
+    const [specialties, setSpecial] = useState("")
 
     useEffect(
         () => {
             fetch("http://localhost:8088/employees")
                 .then(res => res.json())
-                .then((employeeArray) => {
-                    setemployees(employeeArray)
+                .then((employeesFromAPI) => {
+                    changeEmployee(employeesFromAPI)
                 })
         },
         []
     )
 
+    useEffect(() => {
+        const justSpecialities = employees.map(emp => emp.specialty)
+        setSpecial(justSpecialities.join(", "))
+    }, [employees])
+
     return (
         <>
+            <div>
+                Specialties: { specialties }
+            </div>
             {
                 employees.map(
-                    (employeeObject) => {
-                        return <h2 key={`employee--${employeeObject.id}`}>{employeeObject.name}</h2>
+                    (employee) => {
+                        return <p key={`employee--${employee.id}`}>{employee.name}</p>
                     }
                 )
             }
--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 
 export const CustomerList = () => {
     const [customers, setCustomers] = useState([])
+    const [totalCustomerMessage, updateMessage] = useState("")
 
     useEffect(
         () => {
@@ -14,10 +15,23 @@ export const CustomerList = () => {
         []
     )
 
+    useEffect(
+        () => {
+            if (customers.length === 1) {
+                updateMessage("You have 1 customer")
+            }
+            else {
+                updateMessage(`You have ${customers.length} customers`)
+            }
+        },
+        [customers]
+    )
+
     return (
         <>
+            <div>{totalCustomerMessage}</div>
             {
-                customers.map(
+                customers.slice(0, 5).map(
                     (customerObject) => {
                         return <h2 key={`customer--${customerObject.id}`}>{customerObject.name}</h2>
                     }
